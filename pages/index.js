1f98998280f2fa0f195625d97a791de9bdc9072e
supercmmetry <vishaals2000@gmail.com>
2020-11-28 20:57:17 +0530

    feat(meta): add metadata and title
    Signed-off-by: supercmmetry <vishaals2000@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import Head from 'next/head'
 import { FaviconComponent, AppBar, PageScroller, SocialFloatingActionButton, FooterComponent, GithubStats } from '../components'
 
 export default function HomePage () {
   return (
     <>
-      <script src='https://unpkg.com/prevent-pull-refresh' />
+      <Head>
+        <title> supercmmetry </title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='theme-color' content='#000000' />
+        <meta name='description' content='supercmmetry portfolio' />
+        <script src='https://unpkg.com/prevent-pull-refresh' />
+      </Head>
       <FaviconComponent />
       <AppBar />
       <div className='flex flex-row ml-auto mr-auto sm:ml-0 sm:mr-0'>
